2c5ff90a647db74b16abc2132eb56b3851263182
Jessie Ding <nathalie.ding@gmail.com>
2017-06-28 23:37:37 +0800

    refactor query provider

--- src/business/TransQueryProvider.java
+++ src/business/TransQueryProvider.java
@@ -2,112 +2,103 @@ package business;
 
 import java.util.List;
 
-public class TransQueryProvider extends TransAbs {
-	String query_type;// 查询方式
-	String provider_no;// 供货商编号
-	String provider_name;// 供货商名称
+class TransQueryProvider extends TransAbs {
+    private String queryType;// 查询方式
+    private String providerNumber;// 供货商编号
+    private String providerName;// 供货商名称
 
-	public TransQueryProvider() {
-		super();
+    TransQueryProvider() {
+        super();
+    }
 
-	}
+    public void printPrompt() {
+        System.out.println("查询供货商|请输入以下信息：");
+        System.out.println("@queryType @providerNumber @providerName");
+        System.out.println("queryType：0-全部查询  1-精确查询   2-模糊查询");
+    }
 
-	public TransQueryProvider(String query_type, String provider_no, String provider_name) {
-		super();
-		this.query_type = query_type;
-		this.provider_no = provider_no;
-		this.provider_name = provider_name;
-	}
+    public int getInput() {
+        // 读取查询方式
+        queryType = scan.next();
+        if (queryType == null) {
+            setTransResult("获取查询方式失败");
+            return -1;
+        } else if (queryType.equals("0")) {// 全部查询不需要读取剩余的参数
+            return 0;
+        }
 
-	public void printPrompt() {
-		System.out.println("查询供货商|请输入以下信息：");
-		System.out.println("@query_type @providerNumber @providerName");
-		System.out.println("query_type：0-全部查询  1-精确查询   2-模糊查询");
-	}
+        // 供货商编号
+        providerNumber = scan.next();
+        if (providerNumber == null) {
+            setTransResult("读取商品编号错误");
+            return -1;
+        }
 
-	public int getInput() {
-		// 读取查询方式
-		query_type = scan.next();
-		if (query_type == null) {
-			setTransResult("获取查询方式失败");
-			return -1;
-		} else if (query_type.equals("0")) {// 全部查询不需要读取剩余的参数
-			return 0;
-		}
+        providerName = scan.next();
+        if (providerName == null) {
+            setTransResult("读取商品名称错误");
+            return -1;
+        }
+        return 0;
+    }
 
-		// 供货商编号
-		provider_no = scan.next();
-		if (provider_no == null) {
-			setTransResult("读取商品编号错误");
-			return -1;
-		}
+    public int doTrans() {
+        switch (queryType) {
+            case "0": // 全部查询
+                getDbhelper().printAllProvider();
+                setTransResult("查询完成");
+                return 0;
+            case "1": // 精确查询
+                Provider provider = dbhelper.exactFindProvider(providerNumber);
+                if (provider != null) {
+                    System.out.println(provider.toString());
+                    setTransResult("精确查询完成");
+                    return 0;
+                } else {
+                    setTransResult("没有查到相关信息！");
+                    return -1;
+                }
+            case "2": // 模糊查询（目前只能查一个）
+                if (dbhelper.fuzFindProvider(providerName) != null) {
+                    List<Provider> list = dbhelper.fuzFindProvider(providerName);
+                    for (Provider p : list) {
+                        System.out.println(p.toString());
+                    }
+                    return 0;
+                } else {
+                    setTransResult("没有查到相关信息！");
+                    return -1;
+                }
 
-		provider_name = scan.next();
-		if (provider_name == null) {
-			setTransResult("读取商品名称错误");
-			return -1;
-		}
-		return 0;
-	}
+        }
+        return 0;
+    }
 
-	public int doTrans() {
-		if (query_type.equals("0")) {// 全部查询
-			getDbhelper().printAllProvider();
-			setTransResult("查询完成");
-			return 0;
-		} else if (query_type.equals("1")) {// 精确查询
-			Provider provider = dbhelper.exactFindProvider(provider_no);
-			if (provider != null) {
-				System.out.println(provider.toString());
-				setTransResult("精确查询完成");
-				return 0;
-			} else {
-				setTransResult("没有查到相关信息！");
-				return -1;
-			}
-		} else if (query_type.equals("2")) {// 模糊查询（目前只能查一个）
+    public void printResult() {
+        System.out.println(transResult);
+    }
 
-			if (dbhelper.fuzFindProvider(provider_name) != null) {
-				List<Provider> list = dbhelper.fuzFindProvider(provider_name);
-				for (Provider p : list) {
-					System.out.println(p.toString());
-				}
-				return 0;
-			} else {
-				setTransResult("没有查到相关信息！");
-				return -1;
-			}
+    public String getQueryType() {
+        return queryType;
+    }
 
-		}
-		return 0;
-	}
+    public void setQueryType(String queryType) {
+        this.queryType = queryType;
+    }
 
-	public void printResult() {
-		System.out.println(transResult);
-	}
+    public String getProviderNumber() {
+        return providerNumber;
+    }
 
-	public String getQuery_type() {
-		return query_type;
-	}
+    public void setProviderNumber(String providerNumber) {
+        this.providerNumber = providerNumber;
+    }
 
-	public void setQuery_type(String query_type) {
-		this.query_type = query_type;
-	}
+    public String getProviderName() {
+        return providerName;
+    }
 
-	public String getProvider_no() {
-		return provider_no;
-	}
-
-	public void setProvider_no(String provider_no) {
-		this.provider_no = provider_no;
-	}
-
-	public String getProvider_name() {
-		return provider_name;
-	}
-
-	public void setProvider_name(String provider_name) {
-		this.provider_name = provider_name;
-	}
-
+    public void setProviderName(String providerName) {
+        this.providerName = providerName;
+    }
 }
