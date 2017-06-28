8bf7c990ec97640c79acbc76ac22f9469db42dba
Jessie Ding <nathalie.ding@gmail.com>
2017-06-28 23:39:30 +0800

    refactor query inventory

--- src/business/TransQueryInventory.java
+++ src/business/TransQueryInventory.java
@@ -2,37 +2,37 @@ package business;
 
 import java.util.List;
 
-public class TransQueryInventory extends TransAbs {
-	String query_type;
-	String goods_no;// 商品编号
-	String goods_name;// 商品名称
+class TransQueryInventory extends TransAbs {
+	private String queryType;
+	private String goodsNumber;// 商品编号
+	private String goodsName;// 商品名称
 
 	public void printPrompt() {
 		System.out.println("查询商品|请输入以下信息：");
-		System.out.println("@query_type @goods_no @goods_name");
-		System.out.println("query_type：0-全部查询  1-精确查询   2-模糊查询");
+		System.out.println("@queryType @goodsNumber @goodsName");
+		System.out.println("queryType：0-全部查询  1-精确查询   2-模糊查询");
 	}
 
 	public int getInput() {
 		// 读取查询方式
-		query_type = scan.next();
-		if (query_type == null) {
+		queryType = scan.next();
+		if (queryType == null) {
 			setTransResult("获取查询方式失败");
 			return -1;
-		} else if (query_type.equals("0")) {// 全部查询不需要读取剩余的参数
+		} else if (queryType.equals("0")) {// 全部查询不需要读取剩余的参数
 			return 0;
 		}
 
 		// 商品编号
-		goods_no = scan.next();
-		if (goods_no == null) {
+		goodsNumber = scan.next();
+		if (goodsNumber == null) {
 			setTransResult("读取商品编号错误");
 			return -1;
 		}
 
 		// 商品名称
-		goods_name = scan.next();
-		if (goods_name == null) {
+		goodsName = scan.next();
+		if (goodsName == null) {
 			setTransResult("读取商品名称错误");
 			return -1;
 		}
@@ -40,34 +40,34 @@ public class TransQueryInventory extends TransAbs {
 	}
 
 	public int doTrans() {
-		if (query_type.equals("0")) {// 全部查询
-			getDbhelper().printAllInventory();
-			setTransResult("查询完成");
-			return 0;
-		} else if (query_type.equals("1")) {// 精确查询
-			Inventory inventory = dbhelper.exactFindInventory(goods_no);
-			if (inventory != null) {
-				System.out.println(inventory.toString());
-				setTransResult("精确查询完成");
+		switch (queryType) {
+			case "0": // 全部查询
+				getDbhelper().printAllInventory();
+				setTransResult("查询完成");
 				return 0;
-			} else {
-				setTransResult("没有查到相关信息！");
-				return -1;
-			}
-		} else if (query_type.equals("2")) {// 模糊查询
-
-			if (dbhelper.fuzFindInventory(goods_name) != null) {
-				List<Inventory> list = dbhelper.fuzFindInventory(goods_name);
-				for (Inventory i : list) {
-					System.out.println(i.toString());
+			case "1": // 精确查询
+				Inventory inventory = dbhelper.exactFindInventory(goodsNumber);
+				if (inventory != null) {
+					System.out.println(inventory.toString());
+					setTransResult("精确查询完成");
+					return 0;
+				} else {
+					setTransResult("没有查到相关信息！");
+					return -1;
 				}
-				setTransResult("模糊查询完成");
-				return 0;
-			} else {
-				setTransResult("没有查到相关信息！");
-				return -1;
-			}
+			case "2": // 模糊查询
 
+				if (dbhelper.fuzFindInventory(goodsName) != null) {
+					List<Inventory> list = dbhelper.fuzFindInventory(goodsName);
+					for (Inventory i : list) {
+						System.out.println(i.toString());
+					}
+					setTransResult("模糊查询完成");
+					return 0;
+				} else {
+					setTransResult("没有查到相关信息！");
+					return -1;
+				}
 		}
 		return 0;
 	}
