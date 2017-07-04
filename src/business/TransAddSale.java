3bfcd1c87e06e765592f2a9a141da9e377d41e96
Jessie Ding <nathalie.ding@gmail.com>
2017-07-04 22:49:16 +0800

    refactor TransAddSale

--- src/business/TransAddSale.java
+++ src/business/TransAddSale.java
@@ -4,25 +4,22 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class TransAddSale extends TransAbs {
-    String goods_no;// 商品编号
-    String goods_name;// 商品名称
-    int sale_count;// 销售数量
-    String goods_unit;
-    double sale_price;// 销售价格
-    Date sale_date;// 销售日期
-    SalesReport salesReport;
+class TransAddSale extends TransAbs {
+    private String goodsNumber;// 商品编号
+    private String goodsName;// 商品名称
+    private int saleCount;// 销售数量
+    private double salePrice;// 销售价格
+    private Date saleDate;// 销售日期
 
-
-    public int findGoodsInfo(String no) {
+    private int findGoodsInfo(String no) {
         Inventory inventory = databaseOperator.exactFindInventory(no);
         if (inventory == null) {
             setTransResult("库存记录中找不到该商品编号！");
             return -1;
         } else {
             System.out.println("该商品信息如下：");
-            System.out.println("goods_no[" + inventory.getGoodsNumber() + "],");
-            System.out.println("goods_name[" + inventory.getGoodsName() + "],");
+            System.out.println("goodsNumber[" + inventory.getGoodsNumber() + "],");
+            System.out.println("goodsName[" + inventory.getGoodsName() + "],");
             System.out.println("Goods_count[" + inventory.getGoodsCount() + "]");
 
         }
@@ -32,45 +29,44 @@ public class TransAddSale extends TransAbs {
 
     public void printPrompt() {
         System.out.println("输入商品编号：");
-        goods_no = scan.next();
-        if (findGoodsInfo(goods_no) == 0) {
+        goodsNumber = scan.next();
+        if (findGoodsInfo(goodsNumber) == 0) {
             System.out.println("销售录入|请输入以下信息：");
-            System.out.println("@goods_no @goods_name @sale_count @goods_unit @sale_price @sale_date(yyyy-mm-dd)");
+            System.out.println("@goodsNumber @goodsName @saleCount @goods_unit @salePrice @saleDate(yyyy-mm-dd)");
         }
     }
 
     public int getInput() {
-
-        goods_no = scan.next();
-        if (goods_no == null) {
+        goodsNumber = scan.next();
+        if (goodsNumber == null) {
             setTransResult("获取商品编号错误");
             return -1;
         }
 
         // 读取商品名称
-        goods_name = scan.next();
-        if (goods_name == null) {
+        goodsName = scan.next();
+        if (goodsName == null) {
             setTransResult("获取商品名称错误");
             return -1;
         }
 
         // 读取销售数量
         String count = scan.next();
-        sale_count = Integer.parseInt(count);
-        Inventory inventory = databaseOperator.exactFindInventory(goods_no);
-        if (sale_count > inventory.getGoodsCount()) {
+        saleCount = Integer.parseInt(count);
+        Inventory inventory = databaseOperator.exactFindInventory(goodsNumber);
+        if (saleCount > inventory.getGoodsCount()) {
             setTransResult("库存不足！");
             return -1;
         }
 
-        goods_unit = scan.next();
-        if (goods_unit == null) {
+        String goodsUnit = scan.next();
+        if (goodsUnit == null) {
             setTransResult("获取商品单位错误");
             return -1;
         }
 
-        sale_price = Double.parseDouble(scan.next());
-        if (sale_price < inventory.getGoodsPrice()) {
+        salePrice = Double.parseDouble(scan.next());
+        if (salePrice < inventory.getGoodsPrice()) {
             setTransResult("输入错误，售价小于进价！");
             return -1;
         }
@@ -78,10 +74,10 @@ public class TransAddSale extends TransAbs {
         if (scan.hasNext()) {
             DateFormat date = new SimpleDateFormat("yyyy-MM-dd");
             try {
-                sale_date = date.parse(scan.next());
-            } catch (Exception e) {
+                saleDate = date.parse(scan.next());
+            } catch (Exception ignored) {
             }
-            if (sale_date.before(inventory.getPurchaseDate())) {
+            if (saleDate.before(inventory.getPurchaseDate())) {
                 System.out.println("输入错误，销售时间早于进货时间！");
                 return -1;
             }
@@ -94,13 +90,12 @@ public class TransAddSale extends TransAbs {
     }
 
     public int doTrans() {
-
         Sale sale = new Sale();
-        sale.setGoodsNumber(goods_no);
-        sale.setGoodsName(goods_name);
-        sale.setPrice(sale_price);
-        sale.setSaleAmount(sale_count);
-        sale.setSaleDate(sale_date);
+        sale.setGoodsNumber(goodsNumber);
+        sale.setGoodsName(goodsName);
+        sale.setPrice(salePrice);
+        sale.setSaleAmount(saleCount);
+        sale.setSaleDate(saleDate);
         deleteInventory(sale);//从库存中删除
         if (getDatabaseOperator().insertSale(sale) == 0) {
             getDatabaseOperator().printAllSale();
